b72bf8f6a40f261e1cf4a1ad76bb9cab41dcec89
Michael Hoffer <info@michaelhoffer.de>
2013-11-19 17:13:50 +0100

    help implemented

--- VRL-Tutorial-Plugin-04/src/edu/gcsc/vrl/tutorial/TutorialPluginConfigurator.java
+++ VRL-Tutorial-Plugin-04/src/edu/gcsc/vrl/tutorial/TutorialPluginConfigurator.java
@@ -20,7 +20,7 @@ public class TutorialPluginConfigurator extends VPluginConfigurator{
 
     public TutorialPluginConfigurator() {
         //specify the plugin name and version
-       setIdentifier(new PluginIdentifier("Tutorial-Plugin01", "0.1"));
+       setIdentifier(new PluginIdentifier("Tutorial-Plugin04", "0.1"));
 
        // optionally allow other plugins to use the api of this plugin
        // you can specify packages that shall be
@@ -29,7 +29,7 @@ public class TutorialPluginConfigurator extends VPluginConfigurator{
        // exportPackage("com.your.package");
 
        // describe the plugin
-       setDescription("Plugin Description");
+       setDescription("Contains Custom Help Page");
 
        // copyright info
        setCopyrightInfo("Sample-Plugin",
@@ -64,10 +64,7 @@ public class TutorialPluginConfigurator extends VPluginConfigurator{
            // examples:
            //
            // vapi.addComponent(MyComponent.class);
-           // vapi.addTypeRepresentation(MyType.class);
-           
-           vapi.addComponent(TutorialComponent01.class);
-           
+           // vapi.addTypeRepresentation(MyType.class);          
            
            
            // register custom menu actions
